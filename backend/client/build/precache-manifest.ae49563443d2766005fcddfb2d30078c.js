942e1a21a290c76d0d040b11c6bbd547bfdae86e
shaileshsridhar2403 <shailesh.sridhar@gmail.com>
2021-04-06 22:31:20 +0530

    Add client folder back to git repo

---
+++ backend/client/build/precache-manifest.ae49563443d2766005fcddfb2d30078c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a17896b3083b01e4698abf00da6d73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a83d3cafb60b4658cd8",
+    "url": "/static/css/2.9b95f013.chunk.css"
+  },
+  {
+    "revision": "e025512270714e73b724",
+    "url": "/static/css/main.eeed883c.chunk.css"
+  },
+  {
+    "revision": "3a83d3cafb60b4658cd8",
+    "url": "/static/js/2.95a7ffb8.chunk.js"
+  },
+  {
+    "revision": "e025512270714e73b724",
+    "url": "/static/js/main.74e8076d.chunk.js"
+  },
+  {
+    "revision": "2203234bbb04533f74b5",
+    "url": "/static/js/runtime-main.e6ec4625.js"
+  }
+]);
